1833f568a789393c52e52c2126d49f0064631d81
onkar <okal0003@student.monash.edu>
2020-10-23 19:25:38 +1100

    Added file structure made a project in firebase and added google.plist added more View Controllers enabled firebase auth in fire

---
+++ KitchenApp/Utilities/Constants.swift
@@ -0,0 +1,27 @@
+//
+//  Constants.swift
+//  KitchenApp
+//
+//  Created by Techlocker on 23/10/20.
+//
+
+import Foundation
+
+
+class Contants{
+    
+    
+    static func setUserDefaults(email:String?, password:String?){
+        
+        guard let email = email else {return}
+        guard let password = password else {return}
+        
+        let userDefaults = UserDefaults.standard
+        userDefaults.setValue(email, forKey: "email")
+        userDefaults.setValue(password, forKey: "password")
+        
+        
+        
+    }
+    
+}
